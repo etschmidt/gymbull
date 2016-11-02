bbb483ce16b11e376c37b09e80176df8f13e3fe4
Ethan <ethan.t.schmidt@gmail.com>
2016-11-02 23:00:11 +0000

    adds static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Salve Mune"
+    render html: "Salve Munde"
   end
   
 end
