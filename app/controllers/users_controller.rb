0880aa9f86cef7d12b4263c7d2cf8711fb019704
Ethan <ethan.t.schmidt@gmail.com>
2016-11-08 22:13:10 +0000

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,19 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      #successful save
+      flash[:success] = "All Abord the Gym Goat!"
+      redirect_to @user
     else
       render 'new'
     end
   end
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
   
 end
