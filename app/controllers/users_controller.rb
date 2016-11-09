c10b6e20b0bce6d22f0bad4a5cd1f3049784a910
Ethan <ethan.t.schmidt@gmail.com>
2016-11-09 20:35:42 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "All Abord the Gym Goat!"
       redirect_to @user
     else
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,24 @@
+module SessionsHelper
+
+  # Logs in a user
+  def log_in(user)
+      session[:user_id] = user.id
+  end
+  
+  # returns logged in user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  # logs out user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+end
