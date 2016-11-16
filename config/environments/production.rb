725d7f0c8f392bad53cfee4789c185ccbf80a3e2
Ethan <ethan.t.schmidt@gmail.com>
2016-11-16 19:19:28 +0000

    disables check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 end
