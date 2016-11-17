fcfb2294d502ff2d5ab68a13fc81e9846b6657a3
Ethan <ethan.t.schmidt@gmail.com>
2016-11-17 00:04:21 +0000

    adds password resets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115220652) do
+ActiveRecord::Schema.define(version: 20161116225648) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20161115220652) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
