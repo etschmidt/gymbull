d8cd472dc0819644d42d1f532e89f0231f9de9e1
Ethan <ethan.t.schmidt@gmail.com>
2016-11-29 20:19:54 +0000

    crop and resize user pictures

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+    
+  def picture_for(user, options = {})
+    size = options[:size]
+    if user.picture? 
+      image_tag(user.picture, alt: user.name, size: size, class: "picture")
+    else
+      image_tag("gym-clipart.jpg", size: size, class: "picture")
+    end
+  end
+  
 end
