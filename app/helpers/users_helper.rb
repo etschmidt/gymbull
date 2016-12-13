bfd4ff81cfc4a2a7202ab79c6aab446a1adbf2bf
Ethan <ethan.t.schmidt@gmail.com>
2016-12-13 22:01:32 +0000

    adds side nav menu

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,14 +2,21 @@ module UsersHelper
     
   def picture_for(user, options = {})
     size = options[:size]
-    if user.picture? 
-      image_tag(user.picture, alt: user.name, size: size, class: "picture")
+    if gym?(user)
+      if user.picture? 
+        image_tag(user.picture, alt: user.name, size: size, class: "picture")
+      else
+        image_tag("gym-clipart.jpg", size: size, class: "picture")
+      end
     else
-      image_tag("gym-clipart.jpg", size: size, class: "picture")
+      if user.picture? 
+        image_tag(user.picture, alt: user.name, size: size, class: "picture")
+      else
+        image_tag("user-clipart.jpg", size: size, class: "picture")
+      end
     end
   end
   
-    
   #True if user account type is a gym
   def gym?(user)
     user.account_type == "gym"
