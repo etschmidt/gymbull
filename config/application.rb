60225cc307b92a7e76865d3c55c067a8dc9ca518
Ethan <ethan.t.schmidt@gmail.com>
2016-12-25 21:11:32 +0000

    new seeds

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Workspace
     
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
-    
-    config.assets.initialize_on_precompile = false
+
   end
 end
