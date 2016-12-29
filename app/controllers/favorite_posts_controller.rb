fe445988e2c8f30947908ef14e81cb4817add716
Ethan <ethan.t.schmidt@gmail.com>
2016-12-29 19:22:54 +0000

    pseudoAJAX for mires

--- app/controllers/favorite_posts_controller.rb
+++ app/controllers/favorite_posts_controller.rb
@@ -1,11 +1,14 @@
 class FavoritePostsController < ApplicationController
 	before_action :logged_in_user, :set_post
 
+# I wanted these to be proper AJAX calls that just reload partials, but I couldn't
+# figure that out; at least now it doesn't jump to the top of the page
+
   def create
     Favorite.create(favorited: @post, user: current_user)
     respond_to do |format|
       format.html { redirect_to request.referrer }
-      format.js
+      format.js {render inline: "location.reload();" }
     end
   end
   
@@ -13,7 +16,7 @@ class FavoritePostsController < ApplicationController
     Favorite.where(favorited_id: @post.id, user_id: current_user.id).first.destroy
     respond_to do |format|
       format.html { redirect_to request.referrer }
-      format.js
+      format.js {render inline: "location.reload();" }
     end
   end
   
