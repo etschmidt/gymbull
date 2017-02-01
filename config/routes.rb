0befd0685d98ef9b189e4e3075eb908f872b62bf
Ethan <ethan.t.schmidt@gmail.com>
2017-02-01 17:40:44 +0000

    unfucked login logic

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+  post   '/login2',   to: 'sessions#create2'
   delete '/logout',  to: 'sessions#destroy'
   
   get 'tags/:tag', to: 'static_pages#home', as: :tag
