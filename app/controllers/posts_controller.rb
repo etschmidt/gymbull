c16cce01efd5206209c99e977ea5803a0adbf937
Ethan <ethan.t.schmidt@gmail.com>
2017-02-02 18:24:49 +0000

    search w/o login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   
-  before_action :logged_in_user, only: [:create, :destroy, :index]
+  before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,  only: [:edit, :update, :destroy]
     
   def create
