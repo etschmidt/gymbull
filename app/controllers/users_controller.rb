3c5d46d270187a33f4b450689f0faa6568edf748
Ethan <ethan.t.schmidt@gmail.com>
2017-02-13 21:56:11 +0000

    css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,7 @@ class UsersController < ApplicationController
     @title = "Following"
     @user  = User.friendly.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
+    @count = "Following " + @user.following.count.to_s + " users"
     render 'show_follow'
   end
 
@@ -79,6 +80,7 @@ class UsersController < ApplicationController
     @title = "Followers"
     @user = User.friendly.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
+    @count = @user.followers.count.to_s + " followers"
     render 'show_follow'
   end
   
