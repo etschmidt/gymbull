45a96194f008b14756d2e7ed4df6a3bd0fb973e1
Ethan <ethan.t.schmidt@gmail.com>
2017-03-14 17:37:47 +0000

    meta dewscription on posts and profiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
     end
   end
     
+  def meta_description(page_desc ='')
+    base_desc = "A Culture of Fitness. An online 
+                community dedicated to sharing exercise
+                and utrition information around the world"
+    if page_desc.empty?
+      base_desc
+    else
+      page_desc
+    end
+  end
+    
 end
