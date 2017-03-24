d90c16d086a54020f49632b7999d6ce4442e5ee6
Ethan <ethan.t.schmidt@gmail.com>
2017-03-24 19:26:36 +0000

    leonidas

--- app/bots/wodbot_gb.rb
+++ app/bots/wodbot_gb.rb
@@ -1,8 +1,7 @@
 ActiveRecord::Base.connection_pool.with_connection do
 	wod = Wod.new
 	
-	post = Post.new(user_id: [61, 33, 80, 63, 49, 82, 68, 41].sample, 
-						 #user_id: 33,
+	post = Post.new(user_id: 128,
 						 post_type: "workout", 
 						 content: wod.print_ss + wod.print_wod, tag_list: "wod")	
 	post.save
