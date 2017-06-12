e8fa77a8693e595bdb7e2f28e4df901c6b6397df
Ethan <ethan.t.schmidt@gmail.com>
2017-06-12 13:32:23 -0700

    titpic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,11 +14,22 @@ module ApplicationHelper
     base_desc = "A Culture of Fitness. An online 
                 community dedicated to sharing exercise
                 and utrition information around the world"
+
     if page_desc.empty?
       base_desc
     else
       page_desc
     end
   end
-    
+
+  def twitter_pic(page_pic ='')
+    base_pic = "https://www.gymbull.com/assets/BullsHead2-300px-40eb7585a95ae04d8582f8a6335d8418673f2de31f699e4f37aa6af50f4537a7.png"
+
+    if page_pic.empty?
+      base_pic
+    else
+      page_pic
+    end
+  end
+
 end
