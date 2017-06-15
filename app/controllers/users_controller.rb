7530e14dd7ac2a427f0131a2cd110b7d5b20732b
Ethan <ethan.t.schmidt@gmail.com>
2017-06-15 12:27:17 -0700

    no activation emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,8 +45,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      @user.activate
+      log_in @user
+      flash[:success] = "Account activated! Time to get SWOLE!"
       redirect_to root_url
     else
       render 'users/new'
